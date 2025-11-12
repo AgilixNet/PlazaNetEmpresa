0dbeeaf7f0f64dc2321b580244f65f5ab6b265a6
Fabio Andrés Triana Rodriguez <124170936+Fabiotriana07@users.noreply.github.com>
2025-11-12 16:47:46 -0500

    CORS

--- frontend/src/axiosClient.js
+++ frontend/src/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // URL base de tu API
-const API_URL = 'http://localhost:5000';
+const API_URL = 'https://neriah-burriest-sentiently.ngrok-free.dev';
 
 // Crear una instancia de Axios
 const axiosClient = axios.create({
